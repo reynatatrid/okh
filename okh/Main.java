0de45f546f46655ba464726e5d20ac58706f7878
reynatatrid <43331296+reynatatrid@users.noreply.github.com>
2020-04-26 22:57:27 +0700

    file

---
+++ okh/Main.java
@@ -0,0 +1,147 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Muris
+ * Simulasi Timetabling
+ * Modified by reynata
+ */
+//E:\.SEMESTER 6\OKH\.FP\Toronto
+public class Main {
+	//static long startTime = System.nanoTime();
+	static final String DIREKTORI = "D:/Toronto/";
+	
+	public static void main(final String[] args) {
+		String dir_crs = "";
+		String dir_stu = "";
+
+		final String dir_carf92_stu = DIREKTORI + "car-f-92.stu";
+		final String dir_carf92_crs = DIREKTORI + "car-f-92.crs";
+
+		final String dir_cars91_stu = DIREKTORI + "car-s-91.stu";
+		final String dir_cars91_crs = DIREKTORI + "car-s-91.crs";
+
+		final String dir_earf83_stu = DIREKTORI + "ear-f-83.stu";
+		final String dir_earf83_crs = DIREKTORI + "ear-f-83.crs";
+
+		final String dir_hecs92_stu = DIREKTORI + "hec-s-92.stu";
+		final String dir_hecs92_crs = DIREKTORI + "hec-s-92.crs";
+
+		final String dir_kfus93_stu = DIREKTORI + "kfu-s-93.stu";
+		final String dir_kfus93_crs = DIREKTORI + "kfu-s-93.crs";
+
+		final String dir_lsef91_stu = DIREKTORI + "lse-f-91.stu";
+		final String dir_lsef91_crs = DIREKTORI + "lse-f-91.crs";
+
+		final String dir_purs93_stu = DIREKTORI + "pur-s-93.stu";
+		final String dir_purs93_crs = DIREKTORI + "pur-s-93.crs";
+
+		final String dir_ryes93_stu = DIREKTORI + "rye-s-93.stu";
+		final String dir_ryes93_crs = DIREKTORI + "rye-s-93.crs";
+
+		final String dir_staf83_stu = DIREKTORI + "sta-f-83.stu";
+		final String dir_staf83_crs = DIREKTORI + "sta-f-83.crs";
+
+		final String dir_tres92_stu = DIREKTORI + "tre-s-92.stu";
+		final String dir_tres92_crs = DIREKTORI + "tre-s-92.crs";
+
+		final String dir_utas92_stu = DIREKTORI + "uta-s-92.stu";
+		final String dir_utas92_crs = DIREKTORI + "uta-s-92.crs";
+
+		final String dir_utes92_stu = DIREKTORI + "ute-s-92.stu";
+		final String dir_utes92_crs = DIREKTORI + "ute-s-92.crs";
+
+		final String dir_yorf83_stu = DIREKTORI + "yor-f-83.stu";
+		final String dir_yorf83_crs = DIREKTORI + "yor-f-83.crs";
+
+		System.out.println("1. Car-f-92");
+		System.out.println("2. Car-s-91");
+		System.out.println("3. Ear-f-83");
+		System.out.println("4. Hec-s-92");
+		System.out.println("5. Kfu-s-93");
+		System.out.println("6. Lse-f-91");
+		System.out.println("7. Pur-s-93");
+		System.out.println("8. Rye-s-93");
+		System.out.println("9. Sta-f-83");
+		System.out.println("10. Tre-s-92");
+		System.out.println("11. Uta-s-92");
+		System.out.println("12. Ute-s-92");
+		System.out.println("13. yor-f-83");
+		System.out.println("99. EXIT");
+		System.out.print("Pilih dataset : \n");
+
+		final Scanner input = new Scanner(System.in);
+		final int dataset = input.nextInt();
+
+		switch (dataset) {
+			case 1:
+				dir_stu = dir_carf92_stu;
+				dir_crs = dir_carf92_crs;
+				break;
+			case 2:
+				dir_stu = dir_cars91_stu;
+				dir_crs = dir_cars91_crs;
+				break;
+			case 3:
+				dir_stu = dir_earf83_stu;
+				dir_crs = dir_earf83_crs;
+				break;
+			case 4:
+				dir_stu = dir_hecs92_stu;
+				dir_crs = dir_hecs92_crs;
+				break;
+			case 5:
+				dir_stu = dir_kfus93_stu;
+				dir_crs = dir_kfus93_crs;
+				break;
+			case 6:
+				dir_stu = dir_lsef91_stu;
+				dir_crs = dir_lsef91_crs;
+				break;
+			case 7:
+				dir_stu = dir_purs93_stu;
+				dir_crs = dir_purs93_crs;
+				break;
+			case 8:
+				dir_stu = dir_ryes93_stu;
+				dir_crs = dir_ryes93_crs;
+				break;
+			case 9:
+				dir_stu = dir_staf83_stu;
+				dir_crs = dir_staf83_crs;
+				break;
+			case 10:
+				dir_stu = dir_tres92_stu;
+				dir_crs = dir_tres92_crs;
+				break;
+			case 11:
+				dir_stu = dir_utas92_stu;
+				dir_crs = dir_utas92_crs;
+				break;
+			case 12:
+				dir_stu = dir_utes92_stu;
+				dir_crs = dir_utes92_crs;
+				break;
+			case 13:
+				dir_stu = dir_yorf83_stu;
+				dir_crs = dir_yorf83_crs;
+				break;
+			case 99:
+				System.exit(0);
+        }
+        
+        final long startTime0 = System.nanoTime();
+		final CourseSet cs = new CourseSet(dir_crs);
+		final ConflictMatrix cm = new ConflictMatrix(dir_stu, cs.getSize());
+		final int[][] confMat = cm.getConflictMatrix();
+		final int jumlahSiswa = cm.getJumlahStudent();
+		final int[][] solution = Scheduler.getSaturationSchedule(cs.getSize(), cm.getDegree(), confMat);
+		final Solution bestSolution = new Solution(solution);
+		System.out.println("Jumlah Timeslot : " + bestSolution.getJumlahTimeslot());
+		System.out.println("Penalty : " + Utils.getPenalty(confMat, solution, jumlahSiswa));
+		final long endTime0   = System.nanoTime();
+		final long totalTime0 = endTime0 - startTime0;
+		System.out.println("Time (s)  : " + (double)totalTime0/1000000000);
+
+    }
+}
---
+++ okh/Utils.java
@@ -0,0 +1,154 @@
+import java.util.Arrays;
+//import java.util.Random;
+
+public class Utils {
+	public static int[][] copyArray(int[][] arr) {
+		int[][] arrcop = new int[arr.length][arr.length];
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length; j++) {
+				arrcop[i][j] = arr[i][j];
+			}
+		}
+		
+		return arrcop;
+		
+	}
+	
+	public static int[][] copySolution(int[][] arr) {
+		int[][] copySolution = new int[arr.length][2];
+		
+		for(int i = 0; i < arr.length; i++) {
+			copySolution[i][0] = arr[i][0];
+			copySolution[i][1] = arr[i][1];
+		}
+		
+		return copySolution;
+	}
+	
+	public static boolean isNotTabrakan(int[][] matrix, int[][] jadwal) { 
+        for(int course = 0; course < matrix.length-1; course++) {
+        	for (int i = course+1; i < matrix.length; i++) 
+                if (matrix[course][i] != 0 && jadwal[course][1] == jadwal[i][1]) 
+                    return false; 
+        }
+    	
+        return true; 
+    }
+	
+	public static double getPenalty(int[][] matrix, int[][] jadwal, int jumlah) {
+		double penalty = 0;
+		
+		for(int i = 0; i < matrix.length - 1; i++) {
+			for(int j = i+1; j < matrix.length; j++) {
+				if(matrix[i][j] != 0) {
+					if(Math.abs(jadwal[j][1] - jadwal[i][1]) >= 1 && Math.abs(jadwal[j][1] - jadwal[i][1]) <= 5) {
+						penalty = penalty + (matrix[i][j] * (Math.pow(2, 5-(Math.abs(jadwal[j][1] - jadwal[i][1])))));
+					}
+				}
+			}
+		}
+		
+		return penalty/jumlah;
+	}
+	
+	public static int getRandomNumber(int min, int max) {
+//	    Random random = new Random();
+//	    return random.nextInt((max - min) + 1) + min;
+		return (int)(Math.random() * ((max - min) + 1)) + min;
+	}
+	
+	public static int[][] move(int[][] solution, int step) {
+		int[][] temp = solution;
+		int[] timeslot = new int[temp.length];
+		
+		for(int i = 0; i < temp.length; i++) {
+			timeslot[i] = temp[i][1];
+		}
+		
+		for(int i = 0; i < step; i++) {
+			int randomExam = getRandomNumber(1, solution.length-1);
+			int randomTimeslot = getRandomNumber(1,  Arrays.stream(timeslot).max().getAsInt()-1);
+			
+			temp[randomExam][1] = randomTimeslot;
+		}
+		
+		return temp;
+	}
+	
+	public static int[][] swap(int[][] solution, int numSwap) {
+		int[][] temp = solution;
+		
+		for(int i=0; i < numSwap; i++) {
+			int exam1 = getRandomNumber(0, solution.length-1);
+			int exam2 = getRandomNumber(0, solution.length-1);
+			
+			int timeslot1 = solution[exam1][1];
+			int timeslot2 = solution[exam2][1];
+			
+			temp[exam1][1] = timeslot2;
+			temp[exam2][1] = timeslot1;
+		}
+		
+		return temp;
+	}
+	
+	private static int calculateSaturation(int[][] sat, int batas) {
+		int min = 10000;
+		int index = 0;
+		for(int i = 0; i < sat.length; i++) {
+			if(sat[i][1] < min) {
+				index = i;
+				min = sat[i][1];
+			}
+		}
+		return index;
+	}
+	
+	public static int[][] getSaturationSchedule(int size, int[][] largestDegree, int[][] matrix) {
+		int[][] schedule = new int[size][2];
+		int[][] saturation = new int[size][2];
+		int timeslot = 1;
+		
+		for(int i = 0; i<schedule.length; i++) {
+            schedule[i][0] = i+1;
+            schedule[i][1] = -1;
+            saturation[i][0] = largestDegree[i][0];
+            saturation[i][1] = size;
+        }
+		
+		for(int i=0; i<size; i++) {
+            int index = calculateSaturation(saturation, size);
+            for (int j=0; j<=timeslot; j++) {
+                if(isOk(saturation[index][0]-1, j, matrix, schedule, saturation)) {
+                    schedule[saturation[index][0]-1][1] = j;
+                    saturation[index][1] = 100000;
+                    int ind = 0;
+                    for(int k=0; k<matrix.length; k++) {
+                        if(matrix[saturation[index][0]-1][k]!=0) {
+                            ind = k;
+                            for(int l=0; l<saturation.length; l++) {
+                                if(saturation[l][0]==k+1) {
+                                    saturation[l][1] = saturation[l][1]-1;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+                else {
+                    timeslot++;
+                }
+            }
+		}
+		return schedule;
+	}
+		
+	private static boolean isOk(int ex, int jad, int[][]cm, int [][]timeslot, int[][]largest) {
+		for(int i=0; i<cm.length; i++)
+			if(cm[ex][i]!=0 && timeslot[i][1]==jad)
+				return false;
+		return true;
+	}
+	
+}
